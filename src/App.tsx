c8ec298a5d5dc09ec071cdeb7da846494ba6fbb9
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-05-28 10:19:40 -0300

    Feat: adicionando axios e realizando a requisição

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import {useState, useEffect} from 'react'
+import { api } from './lib/api'
 
+interface NotesData {
+  id: number,
+  text: string,
+  date: string
+}
 
 export function App(){
+  const [notes, setNotes] = useState<NotesData[]>()
+
+  async function fetchNotes(){
+    const response = await api.get('notes')
+    try {
+      setNotes(response.data)
+    } catch (error) {
+      return 
+    }
+  }
+
+  console.log(notes);
+  
+  useEffect(() =>{
+    fetchNotes()
+  }, [])
+ 
 
   return (
-    <>
+    <div>
       <h1> Notes App - Hello Word</h1>
-    </>
+    </div>
   )
 }
